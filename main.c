54378561995c1fab4f076fabf7d90627d6dcca25
Mateusz <mateusz@tulip.lol>
2024-09-14 15:12:21 +0200

    a7105 basic config completed

--- main.c
+++ main.c
@@ -228,12 +228,13 @@ int main( void )
 
     A7105_presence_test();
     A7105_reset();
+    A7105_write_reg(0, 0); //As in A7105 Reference code for FIFO mode
     //Crystal A7105= 16MHz=Fxtal
-    // FMS=1(FIFO mode) AIF=1(Auto IF offset for RX)
+    // FMS=1(FIFO mode) AIF=0(Auto IF offset for RX)
     // ARSSI = 1( Enable RSSI measurement in RX mode)
-    A7105_write_reg(1, 0x62);
+    A7105_write_reg(1, 0x42); // mbed, multimodule have 0x42
     A7105_write_reg(2, 0); //Calibs disable
-    A7105_write_reg(3, 0x25); //FIFO End pointer set
+    A7105_write_reg(3, 0x14); //FIFO End pointer set, todo change, 20 bytes
     A7105_write_reg(4, 0); //Fifo pointer margin
     A7105_write_reg(5, 0xff); //FIFO data 
     A7105_write_reg(6, 0xff); //ID Data
@@ -243,7 +244,7 @@ int main( void )
     A7105_write_reg(10, 0); //CKO pin control, SCKI=0(clock non inverted), CKOE=0(high z) 
     //CKOI=0(non inverted) CKOS[3:0]=0(DCK(Data clock) in TX and RCK in RX)
     A7105_write_reg(11, 0x01); //GIO1 control reg, GIO1OE=1(Enabled), GIO1S[3:0]=0 (Wait until RX or TX finished)
-    A7105_write_reg(12, 0x0D);  //GIO2 control reg, GIO2OE=1(enabled pin), GIO2S[3:0]=0b0011 
+    A7105_write_reg(12, 0x0D);  //GIO2 control reg, GIO2OE=1(enabled pin), GIO2S[3:0]=0b0011,there is no plan to use GIO2
     A7105_write_reg(13, 0x05);//Clock register, XS=1(Crystal), CGS=0 (disable internal 32MHz PLL)
     //CSC[1:0]=01(syclk divider Fsys=FMCLK/2=16MHz),GRC[3:0]=0b000(clock generation reference counter)
     A7105_write_reg(14, 0x00);//Data rate register, Data rate = Fsysclk/32/(SDR+1), SDR=0
@@ -259,20 +260,48 @@ int main( void )
     A7105_write_reg(0x15, 0x2b); //TX register 2, FD[4:0]=0xb(frequency deviation), PDV[1:0]=01(should be 01)
     A7105_write_reg(0x16, 0x12); //Delay register 1, PDL[2:0]=2(Delay for TX setting from PLL to WPLL, 10,70us) 2 is recommended val
     //TDL[1:0]=10(reccommended val, 10,60us,delay from WPLL to TX for TX) DPR[2:0]=0(reccomended val, delay scale)
-    A7105_write_reg(0x17, 0x4f); //Delay register 2, RS_DLY[2:0]=0b111(80us, RSSI measurement delay)
-    //AGC_D[1:0]=0b01(20us, AGC delay settling) WSEL[2:0]=0b010(600us, recommended val, xtal settling delay) in my understading with this oscillator is ready in 900us from standby
+    A7105_write_reg(0x17, 0x00); //Delay register 2, RS_DLY[2:0]=0b000(10us, RSSI measurement delay)
+    //AGC_D[1:0]=0b00(10us,recommended val, AGC delay settling) WSEL[2:0]=0b010(200us xtal settling delay)
+    // in my understading with this oscillator is ready in 300+200us=500us from standby
     A7105_write_reg(0x18, 0x62); //0b0110 0010 RX register, ULS=0(Rx up side band)  BWS=1(500kHz bandwidth FIF) DMG=0(should be 0)
     //RXDI=0(RX data output not inverted) FC=0(frequency compensation disabled), RXSM[1:0]=0b11(internal usage should be 11)
     A7105_write_reg(0x19, 0x80);//RX gain register 1, LGC[2:0]=0(LNA gain 24dB), MGC[1:0]=0(mixer gain 24dB),
     //IGC=0(internal should be 0), MVGS=1(recommended val, manual VGA calibrate) VGA(variable gain amplifiers, placed after IF filter)
-    A7105_write_reg(0x1a, 0xff);
-    A7105_write_reg(0x1b, 0xff);
-    A7105_write_reg(0x1c, 0x2a);
-    A7105_write_reg(0x1d, 0x32);
-    A7105_write_reg(0x1e, 0xc3);
-    A7105_write_reg(0x1f, 0x1f);
+    A7105_write_reg(0x1a, 0xff); //RX gain register 2 for internal use, should be 0x0a, multi have 0x80, mbed hax 0x80
+    A7105_write_reg(0x1b, 0xff); //Rx gain register 3 for internal use, should be 0xb4, multi have 0x00, mbed, 0x00
+    A7105_write_reg(0x1c, 0x2a); //Rx gain register 4, for internal use, should be 0x8e, multi,mbed, 0x0a
+    A7105_write_reg(0x1d, 0x32); //RSSI threshold register, 
+    A7105_write_reg(0x1e, 0xc3); //0b1100 0011 ADC Control register, CDM=1(RSSI continuous) RSS=1(RSSI od carrier detect, only one option)
+    //XADS=0 (convert RSS signal, only one option), FSARS=0(ADC clk 4Mhz) ERSS=0(RSSI measuerement until RX mode) RSM[1:0]=11(RSSI margin 20, recommended val)
+    A7105_write_reg(0x1f, 0x0f); //Code register 1, PML[1:0]=11(preamble 4bytes) IDL=1(preamble 4 bytes) CRCS=1(crc enabled)
+    //FECS=0 (FEC disabled) WHTS=0 (data whitening disabled) MCS=0(do not know what is it)
 
+    A7105_write_reg(0x20, 0x13); //Control register 2, PMD[1:0]=11(preamble pattern detection length 16bits)
+    //ETH[1:0]=0(id code error tolerance 0bit) DCL[2:0]=1(demodulator dc estimation average mode, reccomended val)
+    A7105_write_reg(0x21, 0xc3); //Code register 3, WS[6:0]=0xc3 Data whitening seed setting
+    A7105_write_reg(0x22, 0x00); //IF calibration register 1, 0 auto calibration(recommended)
+    A7105_write_reg(0x23, 0xff); //IF calibration register 2, readonly, IF filter calibration deviation
+    A7105_write_reg(0x24, 0); //VCO current calibration, shall be 0, auto calibration
+    A7105_write_reg(0x25, 0); //VCO single band calibration register, 0 means auto
+    A7105_write_reg(0x26, 0x3b); //0b0011 1011 VCO single band calibration register 2, VTL[2:0]=011(recommended val, vco tuning voltage lower threshold 0.4V)
+    //VTH[2:0]=111(recommended val, 1.3V vco tuning voltage upper threshold)
+    A7105_write_reg(0x27, 0); //Battery detect register, BDF=0(detection when battery less than threshold), BDS=0(battery detect disabled)...
+    //BVT[2:0]=0 RGV[1:0]=00(2.1V VVD_A VDD_D voltage setting in non sleep mode, recommended val) RGS=0(VDD_D 3/5*REG_I)
+    A7105_write_reg(0x28, 0x17); //0b0001 0111 TX test register, TBG[2:0]=111(tx buffer setting), PAC[1:0]=10(recommended val, PA current setting)
+    //TCS=0(only one value) Chapter 19, Values means 0.1dBm  
+    A7105_write_reg(0x29, 0x47); //0b0100 0111 RX DEM test register 1, SLF[2:0]=111(recommended val, internal) MPL[1:0]=00(recommended val,internal)
+    //DCM[1:0]=10(Average and hold mode, DC level is average value hold about 8 bit data rate later if preamble detected) DMT=0(shall be 0)
+    A7105_write_reg(0x2a, 0x80); //RX DEM test register 2, (recommended val, demodulator fix dc mode value)
+    A7105_write_reg(0x2b, 0x03); //Charge pump current register, CPC[1:0]=3(reccomended val, 2mA)
+    A7105_write_reg(0x2c, 0x01); //Crystal test register, XC[1:0]=1(shall be 1), XCC=0(shall be 0, internal) DBD=0(shall be 0, internal)
+    A7105_write_reg(0x2d, 0x45); //PLL test register, 0b0100 0101, NSDO=1(sholud be 1, internal) SDPW=0(should be 0, internal)
+    //PRIC[1:0]=01(should be 01, internal), PRRC[1:0]=00(should be 0, internal) PMPE=1(should be 1, internal)
+    A7105_write_reg(0x2e, 0x18); //VCO test register 1, 0b0001 1000 , internal should be as is
+    A7105_write_reg(0x2f, 0); //VCO test register 2, rf analog pin configuration for testing, should be as is
 
+    A7105_write_reg(0x30, 0x01); //IFAT register, internal should be as is
+    A7105_write_reg(0x31, 0x0f); //RScale, internal, should be as is
+    A7105_write_reg(0x32, 0); //Filter test register, internal, should be set as is
 
     while( 1 )
     {
